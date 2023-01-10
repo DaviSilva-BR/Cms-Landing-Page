a33538d9685d742cacab76ce87c5f8dc5478f403
Davi Gabriel Silva <contato@davigabriel.com.br>
2023-01-10 10:41:04 -0300

    View Site

---
+++ resources/views/inc/site/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>FlexStart Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="{{ url('assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ url('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="{{ url('assets/vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+  @include('inc.site.nav')
+
+    @section('content')
+    @endsection
+
+  @include('inc.site.footer')
+
+  <!-- Vendor JS Files -->
+  <script src="{{ url('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ url('assets/vendor/aos/aos.js') }}"></script>
+  <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ url('assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
